3b61176511a2b2b3b72790368e697d243b4d1b70
Kaique-py <66974354+Kaique-py@users.noreply.github.com>
2023-04-25 12:00:16 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -9,7 +9,21 @@ Aplicativo desenvolvido para utilização Desktop.
 - Observar o resultado do IMC do indivíduo
 
 """
+from tkinter import *
+janela = Tk()
+peso_var = float()
+altura_var = float()
 
-def calculaIMC(peso, altura):
+def calculaIMC():
+    peso = peso_var.get()
+    altura = altura_var.get()
     return peso / (altura * altura)
 
+
+janela.title("Calculadora de IMC")
+orientacao_uso = Label(janela, text="Insira os valores do Peso e Altura e, em seguida, clique em Calcular IMC. \n(Utilize ponto ao invés de vírgula para separa casas decimais)")
+orientacao_uso.grid(column=1, row=0)
+botao = Button(janela, text="Calcular IMC", command=calculaIMC)
+botao.grid(column=1, row=2)
+
+janela.mainloop()
